e23183b29c2d012be68c56d3aec51b369a6b9cf0
RafalLeja <rafal@leja.org.pl>
2024-01-18 11:30:31 +0100

    transfered files

---
+++ bezierPaint.py
@@ -0,0 +1,481 @@
+# Rafał Leja 10.01.2024
+
+import tkinter as tk
+from tkinter import ttk
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+from PIL import Image,ImageTk
+import numpy as np
+import utils
+
+class Line:
+  def __init__(self, name, line_type):
+    self.name = name
+    self.line_type = line_type
+    self.points = []
+
+class Point:
+  def __init__(self, x, y):
+    self.x = x
+    self.y = y
+    self.id = np.random.randint(0, 100000)
+
+  def __mul__(self, num):
+    return np.array([self.x * num, self.y * num])
+  
+
+class BezierPaint:
+  def __init__(self, root):
+    self.root = root
+    self.canvas_width = 800
+    self.canvas_height = 800
+    self.canvas = tk.Canvas(self.root, width=self.canvas_width, height=self.canvas_height, bg="white", bd=3, relief=tk.SUNKEN)
+    self.canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+    self.setup_navbar()
+    self.setup_tools()
+    self.setup_events()
+    self.prev_x = None
+    self.prev_y = None
+
+
+  def setup_navbar(self):
+    self.navbar = tk.Menu(self.root)
+    self.root.config(menu=self.navbar)
+
+    # File menu
+    self.file_menu = tk.Menu(self.navbar, tearoff=False)
+    self.navbar.add_cascade(label="File", menu=self.file_menu)
+    self.file_menu.add_command(label="Save lines", command=self.write_lines)
+    self.file_menu.add_command(label="Open lines", command=self.read_lines)
+    self.file_menu.add_separator()
+    self.file_menu.add_command(label="Quit", command=self.root.quit)
+
+    # Edit menu
+    self.edit_menu = tk.Menu(self.navbar, tearoff=False)
+    self.navbar.add_cascade(label="Edit", menu=self.edit_menu)
+    self.edit_menu.add_command(label="Grid", command=self.add_grid)
+    self.edit_menu.add_command(label="Add background", command=self.add_background)
+
+    self.italic_menu = tk.Menu(self.edit_menu, tearoff=False)
+    self.edit_menu.add_cascade(label="Italic", menu=self.italic_menu)
+    self.italic_menu.add_command(label="->", command=lambda: self.italic(1))
+    self.italic_menu.add_command(label="<-", command=lambda: self.italic(-1))
+
+    self.edit_menu.add_command(label="Join Bezier curves", command=self.join_lines_widget)
+    self.edit_menu.add_command(label="Clear canvas", command=self.clear_canvas)
+
+  def setup_tools(self):
+    self.selected_tool = "new"
+
+    self.background_image = None
+    self.grid = False
+    
+    self.line_types = ["Bezier", "Spline", "Color"]
+    self.selected_line_type = self.line_types[0]
+    self.selected_line_name = tk.StringVar()
+    self.selected_line_name.set("New curve")
+
+    self.selected_point = None
+
+    self.lines = [Line(self.selected_line_name.get(), self.selected_line_type)]
+    self.selected_line = self.lines[0]
+
+    self.tool_frame = ttk.LabelFrame(self.root, text="Tools")
+    self.tool_frame.pack(side=tk.RIGHT, padx=5, pady=5, fill=tk.Y)
+
+    self.line_name_label = ttk.Label(self.tool_frame, text="Curve name:")
+    self.line_name_label.pack(side=tk.TOP, padx=5, pady=5)
+
+    self.validate_name = self.tool_frame.register(self.change_line_name)
+    self.line_name_input = ttk.Entry(self.tool_frame, textvariable=self.selected_line_name, validate="all", validatecommand=(self.validate_name, '%P'))
+    self.line_name_input.pack(side=tk.TOP, padx=5, pady=5)
+
+    self.line_type_label = ttk.Label(self.tool_frame, text="Curve type:")
+    self.line_type_label.pack(side=tk.TOP, padx=5, pady=5)
+
+    self.line_type_combobox = ttk.Combobox(self.tool_frame, values=self.line_types, state="readonly", postcommand=self.update_types_box)
+    self.line_type_combobox.current(0)
+    self.line_type_combobox.pack(side=tk.TOP, padx=5, pady=5)
+    self.line_type_combobox.bind("<<ComboboxSelected>>", lambda event: self.select_line_type(self.line_type_combobox.get()))
+
+    self.new_line_button = ttk.Button(self.tool_frame, text="Add new Curve", command=self.add_new_line)
+    self.new_line_button.pack(side=tk.TOP, padx=5, pady=5)
+
+    self.lines_label = ttk.Label(self.tool_frame, text="Curves:")
+    self.lines_label.pack(side=tk.TOP, padx=5, pady=5)
+
+    self.lines_combobox = ttk.Combobox(self.tool_frame, values=[ l.name for l in self.lines ], state="readonly", postcommand=self.update_lines_box)
+    self.lines_combobox.current(0)
+    self.lines_combobox.pack(side=tk.TOP, padx=5, pady=5)
+    self.lines_combobox.bind("<<ComboboxSelected>>", lambda event: self.select_line(self.lines_combobox.get()))
+
+    self.new_point_button = ttk.Button(self.tool_frame, text="Add point", command=self.select_new_point_tool)
+    self.new_point_button.pack(side=tk.TOP, padx=5, pady=5)
+
+    self.dp_point_button = ttk.Button(self.tool_frame, text="Duplicate point", command=self.select_dp_point_tool)
+    self.dp_point_button.pack(side=tk.TOP, padx=5, pady=5)
+
+    self.move_point_button = ttk.Button(self.tool_frame, text="Move point", command=self.select_move_point_tool)
+    self.move_point_button.pack(side=tk.TOP, padx=5, pady=5)
+
+    self.delete_point_button = ttk.Button(self.tool_frame, text="Delete point", command=self.select_delete_point_tool)
+    self.delete_point_button.pack(side=tk.TOP, padx=5, pady=5)
+
+    self.delete_line_button = ttk.Button(self.tool_frame, text="Delete curve", command=self.delete_line)
+    self.delete_line_button.pack(side=tk.TOP, padx=5, pady=5)
+
+  def setup_events(self):
+    self.canvas.bind("<B1-Motion>", self.drawEvent)
+    self.canvas.bind("<Button-1>", self.drawEvent)
+    self.canvas.bind("<ButtonRelease-1>", self.release)
+
+  def change_line_name(self, text):
+    if text in self.lines_combobox['values']:
+      return False
+    self.selected_line.name = text
+    self.update_lines_box()
+    idx = next(i for i,v in enumerate(self.lines) if v.name == text)
+    self.lines_combobox.current(idx)
+    return True
+  
+  def italic(self, direction):
+    R = 0.4
+    midY = self.canvas_height//2
+    for line in self.lines:
+      for i, point in enumerate(line.points):
+        dist = point.y - midY
+        line.points[i].x = point.x - (dist * R)*direction
+    self.draw(-50, -50)
+  
+  def add_grid(self):
+    self.grid = not self.grid
+    self.draw(-50, -50)
+    return
+
+  def add_background(self):
+    filename = askopenfilename()
+    img = ImageTk.PhotoImage(Image.open(filename))
+    self.background_image = img
+    self.draw(-50, -50)
+    return
+
+  def add_new_line(self):
+    count = len(self.lines)
+    name = f'New curve{count}'
+    names = [l.name for l in self.lines]
+    while name in names:
+      count += 1
+      name = f'New curve{count}'
+    line = Line(name, self.line_types[0])
+    self.lines.append(line)
+    self.update_lines_box()
+    self.select_line(line.name)
+    self.lines_combobox.current(len(self.lines)-1)
+
+  def delete_line(self):
+    if len(self.lines) == 1:
+      return
+    idx = next(i for i,v in enumerate(self.lines) if v.name == self.selected_line.name)
+    del self.lines[idx]
+    self.select_line(self.lines[0].name)
+    self.update_lines_box()
+    self.lines_combobox.current(0)
+    return
+
+  def update_lines_box(self):
+    list = [l.name for l in self.lines]
+    self.lines_combobox['values'] = list
+
+  def select_line_type(self, l_type):
+    self.selected_line.line_type = l_type
+    self.selected_line_type = l_type
+    self.draw(-50, -50)
+
+  def update_types_box(self):
+    self.line_type_combobox.current(0 + (self.selected_line_type == "Spline"))
+
+  def select_line(self, line_name):
+    idx = next(i for i,v in enumerate(self.lines) if v.name == line_name)
+    self.selected_line = self.lines[idx]
+    self.selected_line_type = self.lines[idx].line_type
+    self.selected_line_name.set(self.lines[idx].name)
+    self.draw(-50, -50)
+    self.update_types_box()
+
+  def select_new_point_tool(self):
+    self.selected_tool = "new"
+
+  def select_dp_point_tool(self):
+    self.selected_tool = "dp"
+
+  def select_move_point_tool(self):
+    self.selected_tool = "move"
+
+  def select_delete_point_tool(self):
+    self.selected_tool = "delete"
+
+  def drawEvent(self, event):
+    self.draw(event.x , event.y)
+
+  def draw(self, x, y):
+    self.canvas.delete("all")
+    R = 10
+    RES = 300
+    D = 100
+    if self.background_image != None:
+      self.canvas.create_image(0,0,anchor='nw', image=self.background_image)
+    if self.grid:
+      for i in range(0, self.canvas_width, D):
+        self.canvas.create_line(i, 0, i, self.canvas_height, fill="grey")
+      for i in range(0, self.canvas_height, D):
+        self.canvas.create_line(0, i, self.canvas_width, i, fill="grey")
+      midX = ((self.canvas_width/D)//2)*D
+      midY = ((self.canvas_height/D)//2)*D
+      self.canvas.create_line(midX, 0, midX, self.canvas_height, fill="black", width=3)
+      self.canvas.create_line(0, midY, self.canvas_width, midY, fill="black", width=3)
+    for line in self.lines:
+      if self.selected_line == line:
+        if self.selected_tool == "new" and x >= 0:
+          new_point = Point(x, y)
+          line.points = np.append(line.points, new_point)
+          self.selected_point = new_point
+          self.selected_tool = "new_added"
+
+        min_dist = R
+        min_point = None
+        for point in line.points:
+          if self.selected_point == None:
+            dist = np.sqrt((x - point.x)**2+(y - point.y)**2)
+            if dist < min_dist:
+              min_dist = dist
+              min_point = point
+            self.selected_point=min_point
+        for i, point in enumerate(line.points):
+
+          if point == self.selected_point:
+            if self.selected_tool == "dp":
+              new_point = Point(self.selected_point.x - 2*R, self.selected_point.y)
+              self.selected_point.x += 2*R
+              line.points = np.insert(line.points, i, new_point)
+              self.selected_point = new_point
+              self.selected_tool = "dp_added"
+              self.draw(-50, -50)
+            elif self.selected_tool == "delete":
+              self.selected_line.points = np.delete(line.points, i)
+              self.select_move_point_tool()
+              self.draw(-50, -50)
+              return
+            elif self.selected_tool == "move" and x < self.canvas.winfo_width() and y < self.canvas.winfo_height() and x >=0 and y >= 0:
+              self.selected_line.points[i].x = x
+              self.selected_line.points[i].y = y
+
+            if line.line_type == "Color":
+              self.canvas.create_line(point.x-R, point.y-R, point.x+R, point.y+R, fill='green', width=R/3)
+              self.canvas.create_line(point.x-R, point.y+R, point.x+R, point.y-R, fill='green', width=R/3)
+            elif i == 0:
+              self.canvas.create_oval(point.x - R, point.y - R , point.x + R, point.y + R, outline='blue', width=R/3)
+            elif i == len(line.points)-1:
+              self.canvas.create_oval(point.x - R, point.y - R , point.x + R, point.y + R, outline='red', width=R/3)
+            else:
+              self.canvas.create_oval(point.x - R, point.y - R , point.x + R, point.y + R, outline='green', width=R/3)  
+          elif line.line_type == "Color":
+            self.canvas.create_line(point.x-R, point.y-R, point.x+R, point.y+R, fill='green')
+            self.canvas.create_line(point.x-R, point.y+R, point.x+R, point.y-R, fill='green')
+          elif i == 0:
+            self.canvas.create_oval(point.x - R, point.y - R , point.x + R, point.y + R, outline='blue', width=R/3)
+          elif i == len(line.points)-1:
+            self.canvas.create_oval(point.x - R, point.y - R , point.x + R, point.y + R, outline='red', width=R/3)
+          else:        
+            self.canvas.create_oval(point.x - R, point.y - R , point.x + R, point.y + R, outline='black')
+      if len(line.points) > 1:
+        if line.line_type == "Bezier":
+          p = [] 
+          for i in range(RES):
+            p.append(list(utils.casteljau(i/RES, line.points)))
+          self.canvas.create_line(*p)
+        if line.line_type == "Spline":
+          X = [ i.x for i in line.points]
+          Y = [ i.y for i in line.points]
+          mX = utils.interpolMatrix(X)
+          mY = utils.interpolMatrix(Y)
+          p = []
+          for i in range(0, RES+1):
+            p.append(utils.interpolValue(i/RES, X, mX))
+            p.append(utils.interpolValue(i/RES, Y, mY))
+          self.canvas.create_line(*p)
+
+  def release(self, event):
+    if self.selected_tool == "new_added":
+      self.selected_tool = "new"
+    if self.selected_tool == "dp_added":
+      self.selected_tool = "dp"
+    self.selected_point = None
+    self.prev_x = None
+    self.prev_y = None
+
+  def join_lines(self, window, lineA, lineB, startA, startB):
+    window.destroy()
+    if lineA == lineB and startA == startB:
+      return
+    connectionX = (lineA.points[-startA+(startA*-1)-(startA-1)*1].x + lineB.points[-startB+(startB*-1)-(startB-1)*1].x) //2
+    connectionY = (lineA.points[-startA+(startA*-1)-(startA-1)*1].y + lineB.points[-startB+(startB*-1)-(startB-1)*1].y) //2
+    lineB.points[-startB].x = connectionX 
+    lineA.points[-startA].x = connectionX
+    lineB.points[-startB].y = connectionY
+    lineA.points[-startA].y = connectionY
+    self.draw(-50, -50)
+    return
+
+  def join_lines_widget(self):
+    beziers = [l for l in self.lines if l.line_type == "Bezier" and len(l.points) > 2]
+    selectionWindow = tk.Toplevel(self.root)
+    selectionWindow.title("Select the curves you want to join")
+    selectionWindow.geometry("400x200")
+    selectionWindow.resizable(False, False)
+    selectionWindow.grab_set()
+
+    Label = tk.Label(selectionWindow, text="Curves must have at least 3 points, and will join smoothly in the middle of their last points")
+    Label.pack(side=tk.TOP, padx=5, pady=5)
+
+    frameLeft = tk.PanedWindow(selectionWindow, orient=tk.VERTICAL)
+    frameLeft.pack(side=tk.LEFT, padx=5, pady=5)
+
+    frameRight = tk.PanedWindow(selectionWindow, orient=tk.VERTICAL)
+    frameRight.pack(side=tk.RIGHT, padx=5, pady=5)
+
+    frameCenter = tk.PanedWindow(selectionWindow, orient=tk.HORIZONTAL)
+    frameCenter.pack(side=tk.TOP, padx=5, pady=5)
+
+    lineA_start = tk.IntVar()
+    startA = tk.Radiobutton(selectionWindow, text="Start", variable=lineA_start, value=0)
+    frameLeft.add(startA)
+    endA = tk.Radiobutton(selectionWindow, text="End", variable=lineA_start, value=1)
+    frameLeft.add(endA)
+
+    linesA_combobox = ttk.Combobox(selectionWindow, values=[ l.name for l in self.lines ], state="readonly", postcommand=self.update_lines_box)
+    linesA_combobox.current(0)
+    frameLeft.add(linesA_combobox)
+
+    lineB_start = tk.IntVar()
+    startB = tk.Radiobutton(selectionWindow, text="Start", variable=lineB_start, value=0)
+    frameRight.add(startB)
+    endB = tk.Radiobutton(selectionWindow, text="End", variable=lineB_start, value=1)
+    frameRight.add(endB)
+
+    linesB_combobox = ttk.Combobox(selectionWindow, values=[ l.name for l in self.lines ], state="readonly", postcommand=self.update_lines_box)
+    linesB_combobox.current(0)
+    frameRight.add(linesB_combobox)
+
+    frameCenter.add(frameLeft)
+    frameCenter.add(frameRight)
+
+    close_button = tk.Button(selectionWindow, text="Close", command=selectionWindow.destroy)
+    close_button.pack(side=tk.LEFT, padx=5, pady=1)
+
+    ok_button = tk.Button(selectionWindow, text="Join", command=lambda: self.join_lines(selectionWindow, self.lines[linesA_combobox.current()], self.lines[linesB_combobox.current()], lineA_start.get(), lineB_start.get()))
+    ok_button.pack(side=tk.RIGHT, padx=5, pady=1)
+
+    return
+
+  def clear_canvas(self):
+    self.canvas.delete("all")
+    self.lines = [Line(self.selected_line_name.get(), self.selected_line_type)]
+    self.draw(-50, -50)
+
+  def write_lines(self):
+    RES = 100
+    filename = asksaveasfilename()
+    if filename == "":
+      return
+
+    file = open(filename, "wt")
+    width = [self.canvas_width, 0]
+    for line in self.lines:
+      if len(line.points) < 1:
+        continue
+      file.write(f"\"{line.name}\" \"{line.line_type}\":\n  X = [ ")
+      for p in line.points:
+        if p == line.points[-1]:
+          file.write(str(p.x))
+          break
+        file.write(str(p.x) + ", ")
+      file.write("]\n  Y = [ ")
+      for p in line.points:
+        if p == line.points[-1]:
+          file.write(str(p.y))
+          break
+        file.write(str(p.y) + ", ")
+      file.write("]\n")
+      # liczenie szerokosci litery
+      if len(line.points) > 1:
+        if line.line_type == "Bezier":
+          for i in range(RES):
+            x = utils.casteljau(i/RES, line.points)[0]
+            if x > width[1]:
+              width[1] = x
+            if width[0] > x:
+              width[0] = x
+        if line.line_type == "Spline":
+          X = [ i.x for i in line.points]
+          mX = utils.interpolMatrix(X)
+          for i in range(0, RES+1):
+            x = utils.interpolValue(i/RES, X, mX) 
+            if x > width[1]:
+              width[1] = x
+            if width[0] > x:
+              width[0] = x
+    file.write(f"width={int(width[0])},{int(width[1])}")
+    file.close
+
+  def read_lines(self):
+    filename = askopenfilename()
+    if filename == "":
+      return
+
+    file = open(filename, "rt")
+    self.lines = []
+    name = ""
+    pX, pY = [], []
+    idx = 0
+    for line in file.readlines():
+      if line[0] == "\"":
+        if idx > 0:
+          points = [Point(pX[i], pY[i]) for i in range(len(pX))]
+          new_line = Line(name, type_name)
+          new_line.points = points
+          self.lines.append(new_line)
+        nameStart = line.find("\"")
+        nameEnd = line.find("\"", nameStart+1)
+        name = line[nameStart+1:nameEnd]
+        typeStart = line.find("\"", nameEnd+1)
+        typeEnd = line.find("\"", typeStart+1)
+        type_name = line[typeStart+1:typeEnd]
+        idx += 1
+        continue
+      if line[:3] == "  X":
+        pX = []
+        for l in line[8:-2].split(", "):
+          pX.append(int(l))
+        continue
+      if line[:3] == "  Y":
+        pY = []
+        for l in line[8:-2].split(", "):
+          pY.append(int(l))
+        continue
+      if line[0] == "w":
+        continue
+    points = [Point(pX[i], pY[i]) for i in range(len(pX))]
+    new_line = Line(name, type_name)
+    new_line.points = points
+    self.lines.append(new_line)
+    file.close
+    self.select_line(self.lines[0].name)
+    self.update_lines_box()
+    self.select_move_point_tool()
+    self.draw(-50, -50)
+
+if __name__ == "__main__":
+  root = tk.Tk()
+  root.title("bezierPaint")
+  root.resizable(False, False)
+  app = BezierPaint(root)
+  root.mainloop()
---
+++ bezierTyper.py
@@ -0,0 +1,164 @@
+# Rafał Leja 340879
+# konkurs 2 ANL
+
+from PIL import Image, ImageDraw, ImageColor
+import numpy as np
+import utils
+import argparse
+
+def read_curves(src):
+  filename = src
+  try:
+    file = open(filename, "rt")
+  except OSError as e:
+    print(f"Unable to open file {filename}: {e}")
+    exit(1)
+
+  beziers = []
+  nifs = []
+  colors = []
+  pX, pY = [], []
+  width = []
+  type_name = ""
+  idx = 0
+  for line in file.readlines():
+    if line[0] == "\"":
+      if idx > 0:
+        points = np.array([[pX[i], pY[i]] for i in range(len(pX))])
+        if type_name == "Bezier":
+          beziers.append(points)
+        elif type_name == "Spline":
+          nifs.append([pX, pY])
+        elif type_name == "Color":
+          for i in range(len(pX)):
+            colors.append([pX[i], pY[i]])
+
+      nameStart = line.find("\"")
+      nameEnd = line.find("\"", nameStart+1)
+      name = line[nameStart+1:nameEnd]
+      typeStart = line.find("\"", nameEnd+1)
+      typeEnd = line.find("\"", typeStart+1)
+      type_name = line[typeStart+1:typeEnd]
+      idx += 1
+      continue
+    if line[:3] == "  X":
+      pX = []
+      for l in line[8:-2].split(", "):
+        pX.append(int(l))
+      continue
+    if line[:3] == "  Y":
+      pY = []
+      for l in line[8:-2].split(", "):
+        pY.append(int(l))
+      continue
+    if line[0] == "w":
+      for l in line[6:].split(","):
+        width.append(int(l))
+      continue
+
+  points = np.array([[pX[i], pY[i]] for i in range(len(pX))])
+  if type_name == "Bezier":
+    beziers.append(points)
+  elif type_name == "Spline":
+    nifs.append([pX, pY])
+  elif type_name == "Color":
+    for i in range(len(pX)):
+      colors.append([pX[i], pY[i]])
+
+  file.close
+  return [beziers, nifs, colors, width]
+
+
+def draw_letter(letter, img, start, color):
+  RES = 1000
+  beziers, nifs, colors, width = letter
+  for b in beziers:
+    p = [] 
+    for i in range(RES):
+      p.append(tuple(list(utils.casteljau(i/RES, b))))
+      p[i]=tuple([p[i][0]+start[0], p[i][1]+start[1]])
+    img.line(p, fill=color, width=1)
+  for n in nifs:
+    mX = utils.interpolMatrix(n[0])
+    mY = utils.interpolMatrix(n[1])
+    p = []
+    for i in range(0, RES+1):
+      p.append(utils.interpolValue(i/RES, n[0], mX)+start[0])
+      p.append(utils.interpolValue(i/RES, n[1], mY)+start[1])
+    img.line(p, fill=color, width=1)
+
+def main():
+  argparser = argparse.ArgumentParser()
+
+  argparser.add_argument("-s", "--separator_space", type=int, default=50, help="space between letters")
+  argparser.add_argument("-t", "--text", type=str, default="Hello World", help="text to be written")
+  argparser.add_argument("-src", "--source", type=str, default="./letters", help="source directory")
+  argparser.add_argument("-c", "--color", type=str, default="black", help="Text color")
+  argparser.add_argument("-bg", "--background", type=str, default="white", help="Background color")
+  argparser.add_argument("-o", "--output", type=str, default="./text.png", help="output file")
+  argparser.add_argument("-i", "--italic", type=bool, default=False, help="italic font")
+
+  args = argparser.parse_args()
+  
+  letter_width = 800
+  letter_height = 800
+  total_width = 0
+  separator = args.separator_space
+  text = args.text
+  src = args.source
+  color = args.color
+  background = args.background
+
+  text = text.upper()
+
+  letters = dict()
+  if args.italic:
+    total_width += 125
+
+
+  for i in text:
+    if i == " ":
+      total_width += 600
+      continue
+    if i not in letters.keys():
+      letters[i] = read_curves(src+"/"+i+".txt")
+      if args.italic:
+        R = 0.4
+        midY = letter_height/2
+        for line in letters[i][0]:
+          for p in line:
+            dist = p[1] - midY
+            p[0] -= int((dist * R))
+        for line in letters[i][1]:
+          for p in range(len(line[0])):
+            dist = line[1][p] - midY
+            line[0][p] -= int((dist * R))
+        for j in range(len(letters[i][2])):
+          letters[i][2][j][0] -= int((letters[i][2][j][1] - midY) * R)
+
+    total_width += separator + letters[i][3][1] - letters[i][3][0]
+
+
+  with Image.new("RGB", (total_width, letter_height), background) as img:
+    start = [0, 0]
+    if args.italic:
+      start[0] += 70
+    d = ImageDraw.Draw(img)
+    for i in text:
+      if i == " ":
+        start[0] += 600 + separator
+        continue
+      
+      start[0] -= letters[i][3][0]
+     
+      draw_letter(letters[i], d, start, color)
+      for c in letters[i][2]:
+        nc = tuple([c[0] + start[0], c[1]])
+        ImageDraw.floodfill(img, nc, value=ImageColor.getrgb(color))
+
+      start[0] += separator + letters[i][3][1]
+
+    img.save(args.output)
+
+if __name__ == "__main__":
+  main()
